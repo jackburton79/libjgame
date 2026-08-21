c988a9730450ca95118d1cd7aa5c5f5408e128bd
JackBurton79 <stefano.ceccherini@gmail.com>
2026-08-21 14:04:19 +0200

    Removed GameTimer from Timer.cpp/h

--- timers/Timer.h
+++ timers/Timer.h
@@ -5,8 +5,7 @@
  *      Author: stefano
  */
 
-#ifndef __TIMER_H
-#define __TIMER_H
+#pragma once
 
 #include "SupportDefs.h"
 
@@ -58,42 +57,3 @@ private:
 	static timer_map sNamedTimers;
 };
 
-
-class GameTimer {
-public:
-	uint32 Get() const;
-	void SetExpiration(uint32 timer);
-	bool Expired() const;
-	
-	static void Add(const char* name, uint32 expirationTime = -1);
-	static void Remove(const char* name);
-	static GameTimer* Get(const char* string);
-	static uint32 GameTime();
-
-	static uint32 Days();
-	static uint32 Hours();
-	static uint32 Minutes();
-	static uint32 Seconds();
-	static uint16 HourOfDay();
-	static bool IsDayTime();
-
-	static std::string GameTimeString();
-	static void PrintTime();
-	static void UpdateGameTime();
-	static void AdvanceTime(uint32 seconds);
-	static void AdvanceTime(uint16 hours, uint16 minutes, uint16 seconds);
-
-
-private:
-	GameTimer(uint32 expirationTime);
-	//~GameTimer();
-
-	uint32 fExpiration;
-
-	typedef std::map<std::string, GameTimer*> timer_map;
-	static timer_map sTimers;
-	static uint32 sGameTime;
-};
-
-
-#endif /* __TIMER_H */
